5da032123335a4748597024f19027bc685940fb8
Susana Arias <sariaslaso@gmail.com>
2025-01-07 15:51:57 -0500

    imported classes and packages

--- FastAPI/main.py
+++ FastAPI/main.py
@@ -1,6 +1,18 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 
-import sys
-sys.path.append('..')
+#import sys
+#sys.path.append('..')
 
+from Index_Query_Text import IndexQuery
+
+from FlagEmbedding import FlagModel
+model = FlagModel('BAAI/bge-small-zh-v1.5', use_fp16 = True)
+
+from elasticsearch import Elasticsearch
+client = Elasticsearch("http://elasticsearch:9200")
+
+app = FastAPI()
+
+
+text_queries = IndexQuery(client)
